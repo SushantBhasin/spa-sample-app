3aa543b724c490eb5a28991abc17ef97944a1a15
Cezary Czernecki <czarek.czernecki@gmail.com>
2021-02-05 08:44:14 +0100

    Update AEMPage.js

--- src/components/AEMPage.js
+++ src/components/AEMPage.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { AEMText } from './Text';
+import Page from './Page';
 import SampleContent from './SampleContent';
 
 const AEMPage = () => (
     <main>
         <h2>Sample AEM + SPA</h2>
-        <AEMText
-          pagePath="/content/editorxpdevelopment/us/en"
-          itemPath="/root/responsivegrid/text" />
+        <Page pagePath="/editorxpdevelopment/components/structure/page" />
         <SampleContent />
     </main>
 );
 
 export default AEMPage;
+
